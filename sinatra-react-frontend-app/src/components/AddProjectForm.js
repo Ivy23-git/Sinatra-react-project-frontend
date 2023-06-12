b8606760bd43c9ba2f479b8ee8ce30d7f7391189
ivymarubu <ivymarubu@gmail.com>
2023-06-12 13:14:43 +0300

    CSS done

--- sinatra-react-frontend-app/src/components/AddProjectForm.js
+++ sinatra-react-frontend-app/src/components/AddProjectForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './AddProjectForm.css';
 
 const AddProjectForm = () => {
   const [name, setName] = useState('');
--- sinatra-react-frontend-app/src/components/Home.js
+++ sinatra-react-frontend-app/src/components/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Home.css';
 
+
 const Home = () => {
   return (
     <div className="home">
       <h2>Welcome to Project Hub</h2>
       <p>
-        Project Hub is a platform where you can discover and collaborate on various projects. Connect with other like-minded individuals and work together to bring your ideas to life.
-      </p>
-      <p>
+        Project Hub is a platform where you can discover and collaborate on various projects.Connect with other like-minded individuals and work together to bring your ideas to life.
         Start exploring projects or create your own to get started!
       </p>
     </div>
